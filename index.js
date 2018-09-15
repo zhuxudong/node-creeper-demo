178aeb79a29d5be7f6a533bb31214a3d38b5b6aa
zhuxudong <callzhuxudong@163.com>
2018-09-15 09:06:06 +0800

    update

--- index.js
+++ index.js
@@ -3,14 +3,8 @@ let cheerio = require("cheerio");
 let xlsx = require("node-xlsx");
 let superagent = require('superagent-charset');
 
-let url = "http://www.wzfg.com/realweb/stat/ProjectSellingList.jsp";
-let data = [{
-  name: "1",
-  data: [
-    [1, 2, 3],
-    [3, 4, 5]
-  ]
-}];
+let projectUrl = "http://www.wzfg.com/realweb/stat/ProjectSellingList.jsp?";
+let infoUrl = "http://www.wzfg.com/realweb/stat/FirstHandProjectInfo.jsp?"
 
 function formatTimeNow() {
   let time = new Date();
@@ -31,28 +25,50 @@ function get(url) {
   })
 }
 
-function createExcel() {
+function createExcel(filename, data) {
   if (!fs.existsSync("excel")) {
     fs.mkdirSync("excel")
   }
-  fs.writeFileSync("./excel/" + formatTimeNow() + ".xlsx", xlsx.build(data), "binary")
+
+  // fs.writeFileSync("./excel/" + formatTimeNow() + ".xlsx", xlsx.build(data), "binary")
+  fs.writeFileSync("./excel/" + filename + ".xlsx", xlsx.build(data), "binary")
+  console.log("已生成 " + filename + ".xlsx ......");
 }
 
-function getProjectSellingList($) {
-  let projectList = [];
-  return new Promise((resolve, reject) => {
+function getProjectSellingList(pageIndex) {
+  console.log("正在读取第" + (pageIndex + 1) + "页数据")
+  let data = [{
+    name: "项目概况",
+    data: [
+      ["开发单位", "预售许可证号", "发证日期", "所在地区", "样本区域", "项目测算面积", "项目名称", "项目地址", "开盘日期", "售楼地址", "售楼电话"],
+      [3, 4, 5]
+    ]
+  }, {
+    name: "分类价格",
+    data: [
+      [1, 2, 3],
+      [3, 4, 5]
+    ]
+  }, {
+    name: "销售情况",
+    data: [
+      [1, 2, 3],
+      [3, 4, 5]
+    ]
+  }];
+  get(projectUrl + "currPage=" + pageIndex).then(($) => {
     let $trs = $("tr[onclick]");
     $trs.each((i, tr) => {
       let projectName = $(tr).find("td:nth-child(2)").html();
       let onclick = $(tr).attr("onclick");
-      console.log(onclick)
+      let query = onclick.match(/projectID=\d*/)[0];
+      get(infoUrl + query).then(($) => {
+        console.log($(`td:contains('开发单位')`)).html()
+        // createExcel(projectName, data)
+      })
     })
-    resolve();
   })
+
 }
 
-get(url)
-  .then(($) => {
-    return getProjectSellingList($)
-  })
-  .then(createExcel)
+getProjectSellingList(0)
